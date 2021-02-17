5fadb5f7ea414f347832b66335fff2a493850809
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-17 19:30:26 +0100

    First Commit

---
+++ contact_book.py
@@ -0,0 +1,15 @@
+
+class Book:
+    def __init__(self,ime,prezime,e_mail,tel_broj):
+        self.__ime = ime
+        self.__prezime = prezime
+        self.__e_mail = e_mail
+        self.__tel_broj = tel_broj
+
+
+
+    # da se napravi funkcija za pecatenje lista spored e-mail
+    # prima string pr "gmail" ili "yahoo" i treba da gi ispecati site mailovi koi se gmail yahoo itn
+
+    # da se ispecati lista na site broevi koi se na telekom ili one. telekom ==(070,071,072) one == (075,076,077)
+
